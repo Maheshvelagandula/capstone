beff07864b5656f9416d7e0d7234376d4770776b
maheshvelagandula <maheshvelagandula9908@gmail.com>
2022-09-11 16:54:54 +0530

    commit

---
+++ cloths/src/main/java/com/captsone/cloths/entity/Category.java
@@ -0,0 +1,35 @@
+package com.captsone.cloths.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@DynamicInsert
+@DynamicUpdate
+public class Category {
+	
+
+	//@GeneratedValue(strategy = GenerationType.AUTO)
+	@Id
+	private  int category_id;
+	private  String category_name;
+	public int getCategory_id() {
+		return category_id;
+	}
+	public void setCategory_id(int category_id) {
+		this.category_id = category_id;
+	}
+	public String getCategory_name() {
+		return category_name;
+	}
+	public void setCategory_name(String category_name) {
+		this.category_name = category_name;
+	}
+	
+
+}
